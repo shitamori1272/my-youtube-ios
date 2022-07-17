6240ad1c4bdbf602d08ca397f431fcdd05669814
shitamori1272 <33188869+shitamori1272@users.noreply.github.com>
2022-07-17 20:07:54 +0900

    add list view which shows item title

--- MyYoutube/MyYoutube/ContentView.swift
+++ MyYoutube/MyYoutube/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text(presenter.searchListResponse.regionCode)
+            List(presenter.searchListResponse.items) { item in
+                VideoItemRow(videoItem: item)
+            }
         }
     }
 }
